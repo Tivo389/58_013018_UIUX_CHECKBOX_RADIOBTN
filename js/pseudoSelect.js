3827b858dbdc9e64311b501e4314bce234d13010
Shun Miyaki <shunmiyaki@hotmail.com>
2018-02-09 19:53:32 +0900

    git ignore added, initial pseudo code added

---
+++ js/pseudoSelect.js
@@ -0,0 +1,35 @@
+$(document).ready(function() {
+
+
+  function createDropDown(){
+
+      var source = $("#source");
+      var selected = source.find("option[selected]");
+      var options = $("option", source);
+
+      // create <dl> and <dt> with selected value inside it
+      $("body").append('<dl id="target" class="dropdown"></dl>');
+      $("#target").append('<dt><a href="#">' + selected.text() +
+          '<span class="value">' + selected.val() +
+          '</span></a></dt>')
+      $("#target").append('<dd><ul></ul></dd>')
+      // iterate through all the <option> elements and create UL
+      options.each(function(){
+          $("#target dd ul").append('<li><a href="#">' +
+              $(this).text() + '<span class="value">' +
+              $(this).val() + '</span></a></li>');
+      });
+
+  }
+
+  $(".dropdown dd ul li a").click(function() {
+      var text = $(this).html();
+      $(".dropdown dt a").html(text);
+      $(".dropdown dd ul").hide();
+      var source = $("#source");
+      source.val($(this).find("span.value").html())
+  });
+
+  createDropDown()
+
+});
